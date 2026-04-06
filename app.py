4529891faf6d7f53d2ac057fb557a83b7d903fdf
drreginaldovieira74-coder <drreginaldovieira74@gmail.com>
2026-04-06 10:52:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
 import streamlit as st
 import pandas as pd
+def analisar_ciclo(df):
+    todos = set(range(1, 26))
+    ciclo = set()
+    concursos = 0
 
+    for i in range(len(df)-1, -1, -1):
+        linha = set(df.iloc[i].dropna().astype(int))
+        ciclo |= linha
+        concursos += 1
+
+        if ciclo == todos:
+            break
+
+    faltantes = list(todos - ciclo)
+
+    if concursos <= 2:
+        fase = "INÍCIO"
+    elif concursos <= 4:
+        fase = "MEIO"
+    else:
+        fase = "FINAL"
+
+    return concursos, fase, faltantes
+    if arquivo is not None:
+       concursos, fase, faltantes = analisar_ciclo(df)
+
+st.subheader("📊 Análise do Ciclo")
+st.write(f"Concursos no ciclo: {concursos}")
+st.write(f"Fase atual: {fase}")
+st.write(f"Dezenas faltantes: {faltantes}") 
 st.title("🎯 IA Loteria Profissional")
 
 arquivo = st.file_uploader("📂 Envie o CSV da Lotofácil")
